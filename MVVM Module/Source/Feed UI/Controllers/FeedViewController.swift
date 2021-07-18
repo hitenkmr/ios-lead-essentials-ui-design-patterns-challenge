d44b27b581348c6aeeee89578e1468625ed19be9
Hitender kumar <hitender.hk@gmail.com>
2021-07-19 01:31:25 +0530

    MVVM: add error view outlet in FeedViewController and design the same in storyboard

--- MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -5,6 +5,9 @@
 import UIKit
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
+	
+	@IBOutlet weak var errorView: ErrorView!
+	
 	var viewModel: FeedViewModel? {
 		didSet { bind() }
 	}
